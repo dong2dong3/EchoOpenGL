41b5ea68fb7f7d9cfdee0008c8354021ba860ebb
dong2dong3 <765004798@qq.com>
2023-07-04 01:23:09 +0800

    help

--- main.cpp
+++ main.cpp
@@ -45,6 +45,17 @@ GLfloat vertices[] = {
      0.0f,  0.5f, 0.0f  // Top
 };
 
+GLfloat vertices2[] = {
+     0.5f,  0.5f, 0.0f,  // Top Right
+     0.5f, -0.5f, 0.0f,  // Bottom Right
+    -0.5f, -0.5f, 0.0f,  // Bottom Left
+    -0.5f,  0.5f, 0.0f   // Top Left
+};
+GLuint indices[] = {  // Note that we start from 0!
+    0, 1, 3,  // First Triangle
+    1, 2, 3   // Second Triangle
+};
+
 // The MAIN function, from here we start the application and run the game loop
 int main()
 {
@@ -149,7 +160,13 @@ int main()
   glGenBuffers(1, &VBO);
   // 绑定VBO并存储顶点数据
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+  
+  GLuint EBO;
+  glGenBuffers(1, &EBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 
   // 配置顶点属性指针
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
@@ -182,6 +199,7 @@ int main()
     // 绑定VAO并绘制顶点数据
     glBindVertexArray(VAO);
     glDrawArrays(GL_TRIANGLES, 0, 3);
+//    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
 
     // Swap the screen buffers
